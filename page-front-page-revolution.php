f82eb3a89c3cf1e1edc84ee92057ee4fd8a86eb8
Lisa Wells <lisawells999@gmail.com>
2016-08-31 10:52:55 -0400

    slider alt img for mobile

--- page-front-page-revolution.php
+++ page-front-page-revolution.php
@@ -12,9 +12,16 @@ $values = get_post_custom( $post->ID );
 	<section id="abdev_main_slider">
 		<?php
 
-		if ( !wp_is_mobile() ) {
-			/* Display and echo mobile specific stuff here */
+		if ( wp_is_mobile() ) {
 
+			/* Display and echo mobile specific stuff here */
+			/* Our Parallax Slider is 1060x600 */
+			?>
+
+			<div id="mobile-hero"><img src="http://sellup.net/wp-content/uploads/2016/08/SellUp-Email-Marketing-Message-Match_mobl.jpg"></div>
+
+		<?php
+		}	else {
 
 				if( isset( $values['revslider_alias'][0]) && $values['revslider_alias'][0] != '' ){
 					if(function_exists('putRevSlider')){
@@ -24,7 +31,10 @@ $values = get_post_custom( $post->ID );
 				else{
 					_e('You did not select any slider in <i>Front Page Options</i> metabox.', 'ABdev_revelance');
 				}
-		}		?> <!-- end if is mobile -->
+
+
+		}	?> <!-- end if is mobile -->
+
 	</section>
 
 	<?php get_template_part('partials/header_menu'); ?>

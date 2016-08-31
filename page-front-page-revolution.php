f8177e135a2fb22c26adc495343bd8b23b8f4a8e
Lisa Wells <lisawells999@gmail.com>
2016-08-31 09:59:47 -0400

    slider if is not mobile

--- page-front-page-revolution.php
+++ page-front-page-revolution.php
@@ -10,21 +10,26 @@ $values = get_post_custom( $post->ID );
 ?>
 
 	<section id="abdev_main_slider">
-		<?php 
-		if( isset( $values['revslider_alias'][0]) && $values['revslider_alias'][0] != '' ){
-			if(function_exists('putRevSlider')){
-				putRevSlider( $values['revslider_alias'][0] );
-			}
-		}
-		else{
-			_e('You did not select any slider in <i>Front Page Options</i> metabox.', 'ABdev_revelance');
-		}
-		?>
+		<?php
+
+		if ( !wp_is_mobile() ) {
+			/* Display and echo mobile specific stuff here */
+
+
+				if( isset( $values['revslider_alias'][0]) && $values['revslider_alias'][0] != '' ){
+					if(function_exists('putRevSlider')){
+						putRevSlider( $values['revslider_alias'][0] );
+					}
+				}
+				else{
+					_e('You did not select any slider in <i>Front Page Options</i> metabox.', 'ABdev_revelance');
+				}
+		}		?> <!-- end if is mobile -->
 	</section>
 
 	<?php get_template_part('partials/header_menu'); ?>
-	
-	<?php if ( have_posts()) : while (have_posts()) : the_post(); 
+
+	<?php if ( have_posts()) : while (have_posts()) : the_post();
 		the_content();
 	endwhile;
 	endif;

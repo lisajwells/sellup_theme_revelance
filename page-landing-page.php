c706027a114ca7c13e30f8d646340c2682e7f1d0
Lisa Wells <lisawells999@gmail.com>
2016-09-03 15:18:00 -0400

    remove container from landing page so it's like contact page

--- page-landing-page.php
+++ page-landing-page.php
@@ -9,13 +9,11 @@ get_template_part('partials/header_nomenu');
 
 ?>
 	<section class="page_main_section">
-		<div class="container content_fullwidth">
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
 				<?php the_content();?>
 			<?php endwhile; endif;?>
 
-		</div>
 	</section>
 
 <?php get_footer();
